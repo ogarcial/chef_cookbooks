3a83fa866744c1da862b8b56ccde83960b81774e
ChefDK User <chef@ip-10-180-181-143.ec2.internal>
2016-06-23 14:01:53 +0000

    con templates

--- apache/recipes/apache.rb
+++ apache/recipes/apache.rb
@@ -3,11 +3,8 @@ action :install
 end
 
 
-file '/var/www/html/index.html' do
-content "<h1>Hola Mundo desde mi Apache! </h1>
-<h2>ipaddress: #{node['ipaddress']}</h2>
-<h2>hostname: #{node['hostname']}</h2>
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
--- apache/test/integration/default/serverspec/default_spec.rb
+++ apache/test/integration/default/serverspec/default_spec.rb
@@ -4,6 +4,6 @@ describe 'apache::apache' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   describe command('curl localhost') do
-    its(:stdout) { should match /Hola Mundo desde mi Apache!/ }
+    its(:stdout) { should match /Hola Mundo desde mi Apache/ }
   end
 end
