20c8098487eb84aca03a64135061d76924921232
ChefDK User <chef@ip-10-180-181-143.ec2.internal>
2016-06-23 12:57:11 +0000

    con Ips dinamicas

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -8,7 +8,12 @@ user 'chef' do
   action :create
 end
 file '/etc/motd' do
-  content 'Property of Osquitar '
+  content "Property of Osquitar 
+IPADDRESS-ooo: #{node['ipaddress']}
+HOSTNAME :#{node['hostname']} 
+MEMORY : #{node['memory']['total']}
+CPU : #{node['cpu']['0']['mhz']}
+"
   owner 'chef'
 #  owner 'root'
   mode '0644'
